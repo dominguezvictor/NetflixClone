cfa065d3d92a70123d3ff8445739eb9570ea84ad
unknown <dominguezvictor@outlook.com>
2021-04-04 22:41:31 -0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDzkC94lucdy_c9pxPzDU-lf_V7zQgULns",
+    authDomain: "netflix-clone-b7274.firebaseapp.com",
+    projectId: "netflix-clone-b7274",
+    storageBucket: "netflix-clone-b7274.appspot.com",
+    messagingSenderId: "263703488407",
+    appId: "1:263703488407:web:3122ec1d6c4b4d9a3446d2"
+  };
